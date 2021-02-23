8ceb775581d688a2eb30b5a7f2e2157ae747d2ff
zhenia <zhenya.savonenko944@gmail.com>
2021-02-23 13:49:00 +0200

    Initial commit

---
+++ src/main/java/com/nourish1709/project3_security_service/Project3SecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nourish1709.project3_security_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project3SecurityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project3SecurityServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nourish1709/project3_security_service/Project3SecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nourish1709.project3_security_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project3SecurityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
