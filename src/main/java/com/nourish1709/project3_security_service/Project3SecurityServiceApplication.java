e61cc4e66ce991b8225ac5623989c48a95415155
Oleg <kandalov.o.a@gmail.com>
2021-02-26 02:54:26 +0200

    properties - added datasource config pom.xml - added dependency jjwt + web class sbApplication - added annotation model User -> rename @table(users)

--- src/main/java/com/nourish1709/project3_security_service/Project3SecurityServiceApplication.java
+++ src/main/java/com/nourish1709/project3_security_service/Project3SecurityServiceApplication.java
@@ -2,8 +2,10 @@ package com.nourish1709.project3_security_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class Project3SecurityServiceApplication {
 
     public static void main(String[] args) {
